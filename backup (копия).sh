a5674f198f425702eb38f3f4744817183be38752
Alexandr Zaruchevsky <alexandr.zaruchevsky@gmail.com>
2023-03-30 20:43:16 +0300

    Init repo

--- backup (копия).sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker exec mz-app-db-1 sh -c 'exec mysqldump --all-databases -uroot -p$MYSQL_ROOT_PASSWORD' > /home/az/develop/docker-projects/mz-app/mysql/backup/backup-$(date +%Y-%m-%d-%H.%M.%S).sql
